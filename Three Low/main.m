0c134fce63c27757f31a5ed8e85b9cd6973a6cbd
tevinmaker <tevin.maker@gmail.com>
2016-09-07 10:38:40 -0700

    first commit

---
+++ Three Low/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Three Low
+//
+//  Created by Tevin Maker on 2016-09-07.
+//  Copyright © 2016 Tevin Maker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
